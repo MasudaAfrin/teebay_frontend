b6bb2549806dafec7c0d2c1a3fefebeeff6748f6
MasudaAfrin <masudatuba@gmail.com>
2023-09-15 01:12:05 +0600

    user registration api integrate

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,19 +1,44 @@
 import { Row, Col } from 'antd';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { postData } from '../requests/httpServices';
+import { toast } from 'react-toastify';
+
 
 function Registration() {
-    const { register,
-        formState: { errors } } = useForm({
-          defaultValues: {
-            first_name: '',
-            last_name: '',
-            address: '',
-            email: '',
-            password: '',
-            phone_number: '',
-            confirm_password: ''
-          },
-        });
+  const navigate = useNavigate();
+  const { register, handleSubmit,
+    formState: { errors } } = useForm({
+      defaultValues: {
+        first_name: '',
+        last_name: '',
+        address: '',
+        email: '',
+        password: '',
+        phone_number: '',
+        confirm_password: ''
+      },
+    });
+
+    const submitForm = async (data:any) => {
+      const params = {
+        first_name: data.first_name,
+        last_name: data.last_name,
+        address: data.address,
+        phone_number: data.phone_number,
+        email: data.email,
+        password: data.password,
+        confirm_password: data.confirm_password
+      }
+      const response = await postData('api/v1/users', params);
+      if (response.status === 201){
+        console.log('response', response?.data);
+        navigate('/login', { replace: true });
+      } else {
+        console.log(response?.data?.message);
+        toast.error(response?.data?.message);
+      }
+    }
 
   return (
     <div className='w-full'>
@@ -21,13 +46,13 @@ function Registration() {
         <Col span={24}>
           <h2 className='text-xl font-normal text-center tracking-wide'>
             REGISTRATION
-           </h2>
+          </h2>
         </Col>
       </Row>
 
       <Row className='w-1/2 mx-auto border-4 border-[#c5ced6] px-6 py-6 mt-2 2xl:px-12 2xl:py-16 2xl:mt-4'>
         <Row justify={'center'} className='w-full'>
-          <form>
+          <form onSubmit={handleSubmit(submitForm)}>
             <Row gutter={16}>
               <Col span={12}>
                 <input {...register("first_name", { required: 'This is required' })}
@@ -52,14 +77,11 @@ function Registration() {
             </Row>
 
             <Row className='mt-6'>
-            <Col span={24}>
-                <input {...register("address", { required: 'This is required' })}
+              <Col span={24}>
+                <input {...register("address")}
                   type="text"
                   placeholder='Address'
                   className="w-full p-3 border-2 border-[#c5ced6] rounded hover:border-2 hover:border-[#2c88d9] focus:outline-sky-500" />
-                <p className='text-red-500'>
-                  {errors?.address?.message}
-                </p>
               </Col>
             </Row>
 
@@ -87,25 +109,25 @@ function Registration() {
             </Row>
 
             <Row className='mt-6'>
-            <Col span={24}>
+              <Col span={24}>
                 <input {...register("password", { required: 'This is required' })}
                   type="password"
                   placeholder='Password'
                   className="w-full p-3 border-2 border-[#c5ced6] rounded hover:border-2 hover:border-[#2c88d9] focus:outline-sky-500" />
                 <p className='text-red-500'>
-                  {errors?.address?.message}
+                  {errors?.password?.message}
                 </p>
               </Col>
             </Row>
 
             <Row className='mt-6'>
-            <Col span={24}>
+              <Col span={24}>
                 <input {...register("confirm_password", { required: 'This is required' })}
                   type="password"
                   placeholder='Confirm Password'
                   className="w-full p-3 border-2 border-[#c5ced6] rounded hover:border-2 hover:border-[#2c88d9] focus:outline-sky-500" />
                 <p className='text-red-500'>
-                  {errors?.address?.message}
+                  {errors?.confirm_password?.message}
                 </p>
               </Col>
             </Row>
@@ -134,4 +156,4 @@ function Registration() {
   )
 }
 
-export default Registration
+export default Registration;
